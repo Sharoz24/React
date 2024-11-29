0a90cc00b89651b31119934593edf1919c127d73
Sharoz Khan <khansharoz2403@gmail.com>
2024-11-29 08:03:44 +0530

    Ep-12 Part 08

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -7,14 +7,19 @@ const cartSlice = createSlice({
     },
     reducers: {
         addItem: (state, action)=>{
+            // Redux Toolkit uses immer bts
             // mutating the state over here
             state.items.push(action.payload);
         },
         removeItem: (state)=>{
             state.items.pop();
         },
+        //originalState = {items: ["pizza"]}
         clearCart: (state)=>{
-            state.items.length= 0; // []
+            //Redux Toolkit (RTX) - either Mutate the existing state or return a new state
+            //state.items.length = 0; // originalState = []
+            state.items.length= 0; // or "return { items: []}" - both things are same
+            // this new object will be replaced inside originalState = {items: []}
         }
     },
 });
