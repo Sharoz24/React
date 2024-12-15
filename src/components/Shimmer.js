031c560af3a929af83b44c58333432de6bd7f115
Sharoz Khan <khansharoz2403@gmail.com>
2024-12-15 12:58:13 +0530

    Name error

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,23 +1,23 @@
-const Shimmer =()=>{
+const Shimmer = () => {
     return (
-        <div className="shimmer-container" >
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card">Cards</div>
-        </div>
+      <div>
+    <div className="w-64 h-96 p-6 mb-6 bg-gray-300 shadow-lg animate-pulse">
+        <div className="w-full h-48 bg-gray-400 mb-4"></div>
+        <div className="w-3/5 h-4 bg-gray-400 mb-2"></div>
+        <div className="w-4/5 h-4 bg-gray-400 mb-2"></div>
+        <div className="w-full h-4 bg-gray-400"></div>
+      </div>
+
+
+      <div className="w-64 h-96 p-6 mb-6 bg-gray-300 shadow-lg animate-pulse">
+        <div className="w-full h-48 bg-gray-400 mb-4"></div>
+        <div className="w-3/5 h-4 bg-gray-400 mb-2"></div>
+        <div className="w-4/5 h-4 bg-gray-400 mb-2"></div>
+        <div className="w-full h-4 bg-gray-400"></div>
+      </div>
+      </div>
+      
     );
-};
+  }
 
 export default Shimmer;
